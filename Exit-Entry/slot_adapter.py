693abc8459b89c028d666d82719d76728d5cf82f
wangqun <qunge12345@126.com>
2019-01-22 14:50:04 +0800

    modified: upgrade xiongdi's protocol

--- Exit-Entry/slot_adapter.py
+++ Exit-Entry/slot_adapter.py
@@ -65,32 +65,12 @@ class SlotAdapter(object):
         
         return deviceData
 
-    # @staticmethod
-    # def checkXdLoading(vehicle, deviceEvent):
-    #     deviceInfo = deviceEvent.getMachineInfo()   # as str: '2:3:0:1'
-    #     deviceData = list(map(int, deviceInfo.split(':')))
-    #     deviceData = list(map(lambda x: x if x <= 3 else 3, deviceData))
-    #     deviceLoadNum = sum(deviceData)   # one slot on device , mapped 3 slots on vehicle
-    #     availableVehicleSlotNum = vehicle.getAvailableNum()
-        
-    #     # available slot on vehicle is not enough
-    #     if availableVehicleSlotNum < deviceLoadNum:
-    #         SlotAdapter.log.info('no enough passport on vehicle %s' % vehicle.getName())
-    #         return None
-        
-    #     availableVehicleSlotIndexList = vehicle.getAvailableIndexList()
-    #     actualDeviceSlotIndexList = [x for x in [i for i,x in enumerate(deviceData) if x > 0] for i in range(deviceData[x])]
-    #     actualVehicleSlotIndexList = availableVehicleSlotIndexList[:len(actualDeviceSlotIndexList)]
-    #     ret = [(x, actualDeviceSlotIndexList[i]) for i,x in enumerate(actualVehicleSlotIndexList)]
-    #     print(ret)
-    #     return ret
-    
     @staticmethod
     def checkXdLoading(vehicle, deviceEvent):
-        slotRatio = 3    # capability of one device slot / capability of one vehicle slot = 3
-        deviceInfo = deviceEvent.getMachineInfo()   # as str: '3'
-        deviceData = [int(deviceInfo) & 1, (int(deviceInfo) >> 1) & 1]   # change to [1, 0] 
-        deviceLoadNum = sum(deviceData) * slotRatio   # one slot on device , mapped 3 slots on vehicle
+        deviceInfo = deviceEvent.getMachineInfo()   # as str: '2:3:0:1'
+        deviceData = list(map(int, deviceInfo.split(':')))
+        deviceData = list(map(lambda x: x if x <= 3 else 3, deviceData))
+        deviceLoadNum = sum(deviceData)   # one slot on device , mapped 3 slots on vehicle
         availableVehicleSlotNum = vehicle.getAvailableNum()
         
         # available slot on vehicle is not enough
@@ -99,13 +79,33 @@ class SlotAdapter(object):
             return None
         
         availableVehicleSlotIndexList = vehicle.getAvailableIndexList()
-        # deviceSlotIndexList = [i for i,x in enumerate(deviceData) if x > 0]
-        # actualDeviceSlotIndexList = [x for x in deviceSlotIndexList for i in range(slotRatio)]
-        actualDeviceSlotIndexList = [x for x in [i for i,x in enumerate(deviceData) if x > 0] for i in range(slotRatio)]
+        actualDeviceSlotIndexList = [x for x in [i for i,x in enumerate(deviceData) if x > 0] for i in range(deviceData[x])]
         actualVehicleSlotIndexList = availableVehicleSlotIndexList[:len(actualDeviceSlotIndexList)]
         ret = [(x, actualDeviceSlotIndexList[i]) for i,x in enumerate(actualVehicleSlotIndexList)]
         print(ret)
         return ret
+    
+    # @staticmethod
+    # def checkXdLoading(vehicle, deviceEvent):
+    #     slotRatio = 3    # capability of one device slot / capability of one vehicle slot = 3
+    #     deviceInfo = deviceEvent.getMachineInfo()   # as str: '3'
+    #     deviceData = [int(deviceInfo) & 1, (int(deviceInfo) >> 1) & 1]   # change to [1, 0] 
+    #     deviceLoadNum = sum(deviceData) * slotRatio   # one slot on device , mapped 3 slots on vehicle
+    #     availableVehicleSlotNum = vehicle.getAvailableNum()
+        
+    #     # available slot on vehicle is not enough
+    #     if availableVehicleSlotNum < deviceLoadNum:
+    #         SlotAdapter.log.info('no enough passport on vehicle %s' % vehicle.getName())
+    #         return None
+        
+    #     availableVehicleSlotIndexList = vehicle.getAvailableIndexList()
+    #     # deviceSlotIndexList = [i for i,x in enumerate(deviceData) if x > 0]
+    #     # actualDeviceSlotIndexList = [x for x in deviceSlotIndexList for i in range(slotRatio)]
+    #     actualDeviceSlotIndexList = [x for x in [i for i,x in enumerate(deviceData) if x > 0] for i in range(slotRatio)]
+    #     actualVehicleSlotIndexList = availableVehicleSlotIndexList[:len(actualDeviceSlotIndexList)]
+    #     ret = [(x, actualDeviceSlotIndexList[i]) for i,x in enumerate(actualVehicleSlotIndexList)]
+    #     print(ret)
+    #     return ret
 
     @staticmethod
     def checkXdUnloading(vehicle, deviceEvent):
